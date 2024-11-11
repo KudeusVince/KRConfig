04eef91b29fa8450da1b9a5338b811ebfbad28a5
KudeusVince <contact@kudeus.com>
2024-11-11 12:44:57 +0100

    Initial Commit

---
+++ KRConfig/KRConfigApp.swift
@@ -0,0 +1,17 @@
+//
+//  KRConfigApp.swift
+//  KRConfig
+//
+//  Created by vince on 11/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct KRConfigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
